de05f1be8a076522410b4fb9abfe502e60123df2
Akshay <[meherakshay59@gmail.com]>
2022-10-13 17:25:04 +0530

    first Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react' //in react 17 onward updates this statement is not madeterry
+// react arraow fucntion component export ----> rafce shortcut in vs code
+import Resturent from './components/Basics/Resturent'
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Resturent/>
     </div>
-  );
+   
+  )
 }
 
-export default App;
+export default App
